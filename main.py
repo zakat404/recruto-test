f091b06c2a467e152e4c71dc98ca859b117c4cc2
Marius <elevmarius@list.ru>
2024-11-13 20:10:40 +0300

    add virtual

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 @app.get("/")
-def read_root(name: str = "Recruto", message: str = "Давай дружить!"):
-    return f"Hello {name}! {message}"
+async def greet(name: Optional[str] = "Recruto", message: Optional[str] = "Давай дружить"):
+    return {"message": f"Hello {name}! {message}!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
