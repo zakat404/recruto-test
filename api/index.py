7db53235b5a6b8bb04d4730e3aceb4eed7c333c0
Marius <elevmarius@list.ru>
2024-11-13 21:50:01 +0300

    Добавлены новые маршруты

---
+++ api/index.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+
+@app.get("/")
+async def greet(name: Optional[str] = "Recruto", message: Optional[str] = "Давай дружить"):
+    return ({"message": f"Hello {name}! {message}"}
+
+@app.get("/v2"))
+async def greet_v2(name: Optional[str] = "Recruto", message: Optional[str] = "Давай дружить"):
+    return f"Hello {name}! {message}"
