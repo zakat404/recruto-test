ccae5baa6f94c5714a647f2d413827bce6449ce6
Marius <elevmarius@list.ru>
2024-11-13 19:31:14 +0300

    addsad

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from typing import Optional
 
 app = FastAPI()
 
 @app.get("/")
-async def greet(name: Optional[str] = "Recruto", message: Optional[str] = "Давай дружить"):
-    return {"message": f"Hello {name}! {message}!"}
+def read_root(name: str = "Recruto", message: str = "Давай дружить!"):
+    return f"Hello {name}! {message}"
